11531208544f3162765493f6dc72e926525c3ca1
Gabin221 <serrurot19@gmail.com>
2024-07-11 14:02:07 +0200

    Update

--- app/src/main/java/com/example/macavevin/ui/modifier/ModifierFragment.kt
+++ app/src/main/java/com/example/macavevin/ui/modifier/ModifierFragment.kt
@@ -1,14 +1,22 @@
 package com.example.macavevin.ui.modifier
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
+import android.widget.SearchView
+import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.macavevin.Quadruple
 import com.example.macavevin.R
+import com.example.macavevin.VinsViewModel
 import com.example.macavevin.databinding.FragmentModifierBinding
 import com.google.firebase.FirebaseApp
 import com.google.firebase.firestore.FirebaseFirestore
@@ -19,12 +27,20 @@ class ModifierFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var editTextNom: EditText
-    private lateinit var editTextType: EditText
+    private lateinit var spinnerType: Spinner
     private lateinit var editTextRegion: EditText
     private lateinit var editTextAnnee: EditText
     private lateinit var editTextZone: EditText
     private lateinit var editTextQuantite: EditText
     private lateinit var buttonAjouter: Button
+
+    private lateinit var idLV: ListView
+    private lateinit var idSV: SearchView
+
+    private val categories = listOf("Blanc", "Rosé", "Rouge")
+
+    lateinit var typeList: ArrayList<Pair<String, String>>
+    lateinit var listAdapter: ArrayAdapter<Pair<String, String>>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,29 +53,107 @@ class ModifierFragment : Fragment() {
         FirebaseApp.initializeApp(requireActivity())
 
         editTextNom = root.findViewById(R.id.editTextNom)
-        editTextType = root.findViewById(R.id.editTextType)
+        spinnerType = root.findViewById(R.id.spinnerType)
         editTextRegion = root.findViewById(R.id.editTextRegion)
         editTextAnnee = root.findViewById(R.id.editTextAnnee)
         editTextZone = root.findViewById(R.id.editTextZone)
         editTextQuantite = root.findViewById(R.id.editTextQuantite)
         buttonAjouter = root.findViewById(R.id.buttonAjouter)
+        idLV = root.findViewById(R.id.idLV)
+        idSV = root.findViewById(R.id.idSV)
+
+        val listeVinsFiltree = mutableListOf<Pair<String, String>>()
+
+        val spinnerAdapterType = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, listOf("Type de vin", "Blanc", "Rosé", "Rouge"))
+        spinnerAdapterType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerType.adapter = spinnerAdapterType
 
         buttonAjouter.setOnClickListener {
             ajouterOuModifierVin()
         }
 
+        typeList = ArrayList()
+
+        chargerVins()
+
+        idSV.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                listeVinsFiltree.clear()
+                if (newText != null && newText.isNotEmpty()) {
+                    for (film in typeList) {
+                        if (film.first.contains(newText, ignoreCase = true)) {
+                            listeVinsFiltree.add(film)
+                        }
+                    }
+                    idLV.visibility = if (listeVinsFiltree.isEmpty()) View.GONE else View.VISIBLE
+                    // setListViewHeightBasedOnChildren(idLV)
+                } else {
+                    idLV.visibility = View.GONE
+                }
+                listAdapter.notifyDataSetChanged()
+                return false
+            }
+        })
+
         return root
+    }
+
+    private fun chargerVins() {
+        val db = FirebaseFirestore.getInstance()
+
+        for (genre in categories) {
+            val collection = db.collection("Vins").document("Genre").collection(genre)
+            collection.get().addOnSuccessListener { documents ->
+                if (!documents.isEmpty) {
+                    for (document in documents) {
+                        val annee = document.getString("annee") ?: ""
+                        val nom = document.getString("nom") ?: ""
+                        val quantite = document.getString("quantite") ?: ""
+                        val region = document.getString("region") ?: ""
+                        val zone = document.getString("zone") ?: ""
+                        val element = VinsViewModel(annee, nom, quantite, region, zone, genre)
+                        val displayString = element.nom
+                        typeList.add(Pair(displayString, genre))
+                    }
+                }
+            }.addOnFailureListener { e ->
+                Toast.makeText(requireContext(), "Erreur lors du chargement des films de la catégorie $genre : $e", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun supprimerFilm(title: String, category: String) {
+        val db = FirebaseFirestore.getInstance()
+        val collection = db.collection("Vins").document("Genre").collection(category)
+        collection.whereEqualTo("titre", title).get().addOnSuccessListener { documents ->
+            for (document in documents) {
+                collection.document(document.id).delete().addOnSuccessListener {
+                    Toast.makeText(requireContext(), "Vin supprimé avec succès", Toast.LENGTH_SHORT).show()
+                    typeList.removeIf { it.first.contains(title) && it.second == category }
+                    listAdapter.notifyDataSetChanged()
+                    idSV.setQuery("", false)
+                }.addOnFailureListener { e ->
+                    Toast.makeText(requireContext(), "Erreur lors de la suppression du film : $e", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }.addOnFailureListener { e ->
+            Toast.makeText(requireContext(), "Erreur lors de la recherche du film à supprimer : $e", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun ajouterOuModifierVin() {
         val nom = editTextNom.text.toString()
-        val type = editTextType.text.toString()
+        val type = spinnerType.selectedItem.toString()
         val region = editTextRegion.text.toString()
         val annee = editTextAnnee.text.toString()
         val zone = editTextZone.text.toString()
         val quantite = editTextQuantite.text.toString().toIntOrNull() ?: 1
 
-        if (nom.isBlank() || type.isBlank() || region.isBlank() || annee.isBlank() || zone.isBlank() || quantite <= 0) {
+        if (nom.isBlank() || type.equals("Type") || region.isBlank() || annee.isBlank() || zone.isBlank() || quantite <= 0) {
             Toast.makeText(requireContext(), "Tous les champs doivent être remplis", Toast.LENGTH_SHORT).show()
             return
         }
@@ -76,12 +170,11 @@ class ModifierFragment : Fragment() {
                 if (documents.isEmpty) {
                     // Ajouter un nouveau document
                     val newVin = hashMapOf(
-                        "nom" to nom,
-                        "type" to type,
-                        "region" to region,
                         "annee" to annee,
-                        "zone" to zone,
-                        "quantite" to quantite.toString()
+                        "nom" to nom,
+                        "quantite" to quantite.toString(),
+                        "region" to region,
+                        "zone" to zone
                     )
 
                     collectionRef.add(newVin)
@@ -116,7 +209,7 @@ class ModifierFragment : Fragment() {
 
     private fun clearFields() {
         editTextNom.text.clear()
-        editTextType.text.clear()
+        spinnerType.setSelection(0)
         editTextRegion.text.clear()
         editTextAnnee.text.clear()
         editTextZone.text.clear()
