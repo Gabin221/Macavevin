05da974bd563e663c2a05ad6884ddce5f6b8352d
Gabin221 <serrurot19@gmail.com>
2024-07-11 15:19:56 +0200

    Update

--- app/src/main/java/com/example/macavevin/ui/modifier/ModifierFragment.kt
+++ app/src/main/java/com/example/macavevin/ui/modifier/ModifierFragment.kt
@@ -1,6 +1,5 @@
 package com.example.macavevin.ui.modifier
 
-import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,12 +10,9 @@ import android.widget.EditText
 import android.widget.ListView
 import android.widget.SearchView
 import android.widget.Spinner
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.macavevin.Quadruple
 import com.example.macavevin.R
-import com.example.macavevin.VinsViewModel
 import com.example.macavevin.databinding.FragmentModifierBinding
 import com.google.firebase.FirebaseApp
 import com.google.firebase.firestore.FirebaseFirestore
@@ -37,10 +33,9 @@ class ModifierFragment : Fragment() {
     private lateinit var idLV: ListView
     private lateinit var idSV: SearchView
 
+    private lateinit var listAdapter: ArrayAdapter<Pair<String, String>>
+    private var vinsList = ArrayList<Pair<String, String>>()
     private val categories = listOf("Blanc", "Rosé", "Rouge")
-
-    lateinit var typeList: ArrayList<Pair<String, String>>
-    lateinit var listAdapter: ArrayAdapter<Pair<String, String>>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -62,8 +57,6 @@ class ModifierFragment : Fragment() {
         idLV = root.findViewById(R.id.idLV)
         idSV = root.findViewById(R.id.idSV)
 
-        val listeVinsFiltree = mutableListOf<Pair<String, String>>()
-
         val spinnerAdapterType = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, listOf("Type de vin", "Blanc", "Rosé", "Rouge"))
         spinnerAdapterType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerType.adapter = spinnerAdapterType
@@ -72,77 +65,7 @@ class ModifierFragment : Fragment() {
             ajouterOuModifierVin()
         }
 
-        typeList = ArrayList()
-
-        chargerVins()
-
-        idSV.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                listeVinsFiltree.clear()
-                if (newText != null && newText.isNotEmpty()) {
-                    for (film in typeList) {
-                        if (film.first.contains(newText, ignoreCase = true)) {
-                            listeVinsFiltree.add(film)
-                        }
-                    }
-                    idLV.visibility = if (listeVinsFiltree.isEmpty()) View.GONE else View.VISIBLE
-                    // setListViewHeightBasedOnChildren(idLV)
-                } else {
-                    idLV.visibility = View.GONE
-                }
-                listAdapter.notifyDataSetChanged()
-                return false
-            }
-        })
-
         return root
-    }
-
-    private fun chargerVins() {
-        val db = FirebaseFirestore.getInstance()
-
-        for (genre in categories) {
-            val collection = db.collection("Vins").document("Genre").collection(genre)
-            collection.get().addOnSuccessListener { documents ->
-                if (!documents.isEmpty) {
-                    for (document in documents) {
-                        val annee = document.getString("annee") ?: ""
-                        val nom = document.getString("nom") ?: ""
-                        val quantite = document.getString("quantite") ?: ""
-                        val region = document.getString("region") ?: ""
-                        val zone = document.getString("zone") ?: ""
-                        val element = VinsViewModel(annee, nom, quantite, region, zone, genre)
-                        val displayString = element.nom
-                        typeList.add(Pair(displayString, genre))
-                    }
-                }
-            }.addOnFailureListener { e ->
-                Toast.makeText(requireContext(), "Erreur lors du chargement des films de la catégorie $genre : $e", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    private fun supprimerFilm(title: String, category: String) {
-        val db = FirebaseFirestore.getInstance()
-        val collection = db.collection("Vins").document("Genre").collection(category)
-        collection.whereEqualTo("titre", title).get().addOnSuccessListener { documents ->
-            for (document in documents) {
-                collection.document(document.id).delete().addOnSuccessListener {
-                    Toast.makeText(requireContext(), "Vin supprimé avec succès", Toast.LENGTH_SHORT).show()
-                    typeList.removeIf { it.first.contains(title) && it.second == category }
-                    listAdapter.notifyDataSetChanged()
-                    idSV.setQuery("", false)
-                }.addOnFailureListener { e ->
-                    Toast.makeText(requireContext(), "Erreur lors de la suppression du film : $e", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }.addOnFailureListener { e ->
-            Toast.makeText(requireContext(), "Erreur lors de la recherche du film à supprimer : $e", Toast.LENGTH_SHORT).show()
-        }
     }
 
     private fun ajouterOuModifierVin() {
